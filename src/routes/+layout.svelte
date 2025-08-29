a89d099c7a999ca9f346913a6a49f8b3ae76c496
nshenker <64337739+nshenker@users.noreply.github.com>
2025-08-29 10:30:28 -0400

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,8 +7,6 @@
     import ThemeSwitcher from "./ThemeSwitcher.svelte";
     import { goto } from '$app/navigation';
 
-    let isFullscreen;
-
     theme.subscribe(value => {
         if (browser) {
             document.documentElement.setAttribute('data-theme', value);
@@ -22,7 +20,7 @@
     });
 </script>
 
-<Fullscreen bind:fullscreen={isFullscreen}>
+<Fullscreen bind:fullscreen={$fullScreen}>
     <div class="bg-base-200 min-h-screen">
         <div class="fixed top-0 left-0 right-0 z-50 bg-base-100 shadow-md no-print">
             <div class="navbar px-4">
@@ -44,9 +42,9 @@
                 <div class="navbar-end">
                     <ThemeSwitcher />
                     {#if browser}
-                        <button class="btn btn-ghost btn-circle" on:click={() => isFullscreen = !isFullscreen}>
-                            {#if !isFullscreen}
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 inline"><path stroke-linecap="round" stroke-linejoin="round" d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15" /></svg>
+                        <button class="btn btn-ghost btn-circle" on:click={() => $fullScreen = !$fullScreen}>
+                            {#if !$fullScreen}
+                                <svg xmlns="http://www.w.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 inline"><path stroke-linecap="round" stroke-linejoin="round" d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15" /></svg>
                             {:else}
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 inline"><path stroke-linecap="round" stroke-linejoin="round" d="M9 9V4.5M9 9H4.5M9 9L3.75 3.75M9 15v4.5M9 15H4.5M9 15l-5.25 5.25M15 9h4.5M15 9V4.5M15 9l5.25-5.25M15 15h4.5M15 15v4.5m0-4.5l5.25 5.25" /></svg>
                             {/if}
