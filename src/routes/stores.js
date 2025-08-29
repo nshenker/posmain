06bd2b93e554ae29ec91dfb27b6e92e47b90a39a
nshenker <64337739+nshenker@users.noreply.github.com>
2025-08-29 01:16:41 -0400

    Update stores.js

--- src/routes/stores.js
+++ src/routes/stores.js
@@ -24,14 +24,14 @@ export const mostRecentTxn = createPersistentStore("mostRecentTxn", "");
 export const showWarning = createPersistentStore("showWarning", true);
 export const fullScreen = createPersistentStore("fullScreen", false);
 export let successArray = createPersistentStore("successArray", []);
+
+// Simplified the mints array to only include USDC, SOL, and BONK
 export const mints = writable([
     {name:"USDC", mint:"EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"},
-    {name:"DUST", mint:"DUSTawucrTsGU8hcqRdHDCbuYhCPADMLM2VcCb8VnFnQ"},
-    {name:"FOXY", mint:"FoXyMu5xwXre7zEoSvzViRk3nGawHUp9kUh97y2NDhcq"},
-    {name:"JOINTS", mint:"7ftKSttU6yUAnWsWxpRP3LKdQNEto8V4KD9NuWttoVnV"},
-    {name:"USDT", mint:"Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB"},
     {name:"SOL", mint:"So11111111111111111111111111111111111111112"},
-    {name:"BONK", mint:"DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263"}]);
+    {name:"BONK", mint:"DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263"}
+]);
+
 export const selectedMint = createPersistentStore("selectedMint", "USDC");
 export const merchantLogo = createPersistentStore("merchantLogo", "");
 export const theme = createPersistentStore("theme", "light");
