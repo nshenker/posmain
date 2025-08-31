fbf8cbd438a15af9ed59049232491881582d1411
nshenker <64337739+nshenker@users.noreply.github.com>
2025-08-31 16:28:37 -0400

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,7 @@
     import { onMount, onDestroy } from 'svelte';
     import { browser } from '$app/environment';
     import "../app.css";
-    import { merchantLogo, theme } from './stores.js';
-    import { fullscreen } from './fullscreenStore.js';
+    import { fullScreen, merchantLogo, theme } from './stores.js';
     import ThemeSwitcher from "./ThemeSwitcher.svelte";
     import { goto } from '$app/navigation';
     import { Toaster } from 'svelte-french-toast';
@@ -16,7 +15,20 @@
     });
 
     function handleFullscreenChange() {
-        fullscreen.set(document.fullscreenElement != null);
+        const isCurrentlyFullscreen = document.fullscreenElement != null;
+        if ($fullScreen !== isCurrentlyFullscreen) {
+            fullScreen.set(isCurrentlyFullscreen);
+        }
+    }
+
+    async function toggleFullscreen() {
+        if (!document.fullscreenElement) {
+            await document.documentElement.requestFullscreen();
+        } else {
+            if (document.exitFullscreen) {
+                await document.exitFullscreen();
+            }
+        }
     }
 
     onMount(() => {
@@ -57,8 +69,8 @@
             <div class="navbar-end">
                 <ThemeSwitcher />
                 {#if browser}
-                    <button class="btn btn-ghost btn-circle" on:click={fullscreen.toggle}>
-                        {#if !$fullscreen}
+                    <button class="btn btn-ghost btn-circle" on:click={toggleFullscreen}>
+                        {#if !$fullScreen}
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 inline"><path stroke-linecap="round" stroke-linejoin="round" d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15" /></svg>
                         {:else}
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 inline"><path stroke-linecap="round" stroke-linejoin="round" d="M9 9V4.5M9 9H4.5M9 9L3.75 3.75M9 15v4.5M9 15H4.5M9 15l-5.25 5.25M15 9h4.5M15 9V4.5M15 9l5.25-5.25M15 15h4.5M15 15v4.5m0-4.5l5.25 5.25" /></svg>
